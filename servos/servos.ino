1346d47b057f2079f927794ecafa9073842d1b74
Gerrit Neven <gerrit@hertzian.co.uk>
2017-01-13 00:59:58 +0000

    add methods to control hand, save angles as presets

--- servos/servos.ino
+++ servos/servos.ino
@@ -12,6 +12,15 @@
 Servo servos[4];
 int servoPins[4] = {9, 8, 7, 6};
 int ledPin = 13;
+// 1 is pink/ring
+// 2 is thumb
+// 3 is pointy finger
+// 4 is middle finger
+
+int anglesClosed[4] = {90, 90, 90, 100};
+int anglesHandShake[4] = {60, 60, 60, 70};
+
+bool closed = false;
 
 int pos = 0;    // variable to store the servo position
 
@@ -26,14 +35,36 @@ void setup() {
 }
 
 void loop() {
+  openHand();
+  delay(5000);
+  if (!closed) {
+    handShake();
+    closed = true;
+  } else {
+    openHand();
+  }
+  delay(3000);
+//  handShake();
+}
+
+void handShake() {
+  for (int i = 0; i < 4; i++) {
+    rotate(i+1, anglesHandShake[i]);
+  }
+  delay(2000);
+  openHand();
+}
+
+void closeHand() {
+  for (int i = 0; i < 4; i++) {
+    rotate(i+1, anglesClosed[i]);
+  }
+}
+
+void openHand () {
   for (int i = 0; i < 4; i++) {
     rotate(i + 1, 0);
   }
-  delay(1000);
-//  for (int i = 0; i < 4; i++) {
-//    rotate(i + 1, 180);
-//  }
-  delay(1000);
 }
 
 void rotate(int finger, int degrees) {

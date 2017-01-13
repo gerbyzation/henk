0e2676cf69556f6fa0118b54433bdd97e117e931
Gerrit Neven <gerrit@hertzian.co.uk>
2017-01-13 01:00:17 +0000

    count detections in sequence

--- capacitiveSensor/capacitiveSensor.ino
+++ capacitiveSensor/capacitiveSensor.ino
@@ -9,6 +9,8 @@ boolean previous = false;
 
 int debounce = 200;
 
+int nDetects = 0;
+
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired// To add more sensors...//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
 void setup() {
   Serial.begin(9600);
@@ -20,27 +22,35 @@ void setup() {
 void loop() {
   long total1 =  cs_4_2.capacitiveSensor(30);
   
-  if (total1 > 60){yes = true;}
-  else {yes = false;}    
+  if (total1 > 60) {
+    yes = true;
+    nDetects++;
+  }
+  else {
+    yes = false;
+    nDetects = 0;
+  }    
   
   // to toggle the state of state
-  if(yes == true  && millis() - time>debounce){
-    
+  if(yes == true  && millis() - time>debounce) {
     if(state == LOW) {
       state = HIGH;
     } else {
-       state = LOW;
+//       state = LOW;
        time = millis();     
     }
+  } else {
+    state = LOW;
   }
-     
- 
+
   digitalWrite(led, state);
   previous = yes;
 
   Serial.print("time ");
   Serial.print(millis()-time);
   Serial.print(" the other thing ");
-  Serial.println(total1);
+  Serial.print(total1);
+  Serial.print(" detects ");
+  Serial.println(nDetects);
   delay(10);
 }
